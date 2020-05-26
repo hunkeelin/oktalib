a17423532638a35be53934756897ec33d3b386dd
Keith H. Lin <keithlin.732@gmail.com>
2020-05-25 21:43:41 -0700

    fix constant golint

--- const.go
+++ const.go
@@ -1,7 +1,7 @@
 package oktalib
 
 const (
-	MfaCode = "token:software:totp" //MfaCode
-	YubiKey = "token:hardware"      //YubiKey
-	MfaPush = "push"                //MfaPush
+	MfaCode = "token:software:totp" //MfaCode mfa code constant
+	YubiKey = "token:hardware"      //YubiKey hardware mfa constant
+	MfaPush = "push"                //MfaPush push mfa constant
 )
