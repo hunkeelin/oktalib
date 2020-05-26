a7bbeeb08e3c9873fcf360cc89835b469571f7a2
Keith H. Lin <keithlin.732@gmail.com>
2020-05-25 21:44:27 -0700

    what's going on

--- const.go
+++ const.go
@@ -1,7 +1,7 @@
 package oktalib
 
 const (
-	MfaCode = "token:software:totp" //MfaCode mfa code constant
 	YubiKey = "token:hardware"      //YubiKey hardware mfa constant
 	MfaPush = "push"                //MfaPush push mfa constant
+	MfaCode = "token:software:totp" //MfaCode mfa code constant
 )
